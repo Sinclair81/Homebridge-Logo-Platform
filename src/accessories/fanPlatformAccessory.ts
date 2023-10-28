e19ae41309c78f61ddeccbe465646999fc538fed
Sinclair81 <dev.sinclair81@gmail.com>
2023-10-28 10:34:33 +0200

    Update fanPlatformAccessory.ts

--- src/accessories/fanPlatformAccessory.ts
+++ src/accessories/fanPlatformAccessory.ts
@@ -117,9 +117,9 @@ export class FanPlatformAccessory implements AccessoryPlugin {
 
       let qItem: QueueSendItem;
       if (value) {
-        qItem = new QueueSendItem(this.device.fanSetRotationDirectionCW, 1, this.pushButton);
+        qItem = new QueueSendItem(this.device.fanSetRotationDirectionCW, value, this.pushButton);
       } else {
-        qItem = new QueueSendItem(this.device.fanSetRotationDirectionCCW, 1, this.pushButton);
+        qItem = new QueueSendItem(this.device.fanSetRotationDirectionCCW, value, this.pushButton);
       }
       this.platform.queue.bequeue(qItem);
       
