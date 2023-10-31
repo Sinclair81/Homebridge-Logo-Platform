849755cbbb191905f80474e1f42afb27f8e9ad57
Sinclair81 <dev.sinclair81@gmail.com>
2023-10-31 18:11:24 +0100

    Update switchPlatformAccessory.ts

--- src/accessories/switchPlatformAccessory.ts
+++ src/accessories/switchPlatformAccessory.ts
@@ -32,7 +32,7 @@ export class SwitchPlatformAccessory implements AccessoryPlugin {
     this.api        = api;
     this.platform   = platform;
     this.device     = device;
-    this.pushButton = (this.device.pushButton ? 1 : 0) || this.platform.pushButton;
+    this.pushButton = this.device.pushButton || this.platform.pushButton;
 
     this.errorCheck();
 
