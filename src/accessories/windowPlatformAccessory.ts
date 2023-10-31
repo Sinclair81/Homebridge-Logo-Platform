ee5639574467582539855512e33b8a0864d2c8b5
Sinclair81 <dev.sinclair81@gmail.com>
2023-10-31 18:12:27 +0100

    Update windowPlatformAccessory.ts

--- src/accessories/windowPlatformAccessory.ts
+++ src/accessories/windowPlatformAccessory.ts
@@ -36,7 +36,7 @@ export class WindowPlatformAccessory implements AccessoryPlugin {
     this.api        = api;
     this.platform   = platform;
     this.device     = device;
-    this.pushButton = (this.device.pushButton ? 1 : 0) || this.platform.pushButton;
+    this.pushButton = this.device.pushButton || this.platform.pushButton;
 
     this.errorCheck();
     this.currentPositionIsTargetPositionInLogo = this.checkPosition();
